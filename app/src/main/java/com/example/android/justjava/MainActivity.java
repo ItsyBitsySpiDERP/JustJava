bf103e5c294170618e72bd9ee4c315651bcfacab
Kristen Vincenti <37001304+ItsyBitsySpiDERP@users.noreply.github.com>
2018-08-28 21:48:22 -0400

    Scrolling added and more options for coffee
    Can now scroll in landscape mode and can now add toppings to order.

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -3,14 +3,13 @@ package com.example.android.justjava;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
 
 
-/**
- * This app displays an order form to order coffee.
- */
+/**This app displays an order form to order coffee.*/
 public class MainActivity extends AppCompatActivity {
     int quantity = 0;
 
@@ -18,59 +17,60 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    /**
-     * This method is called when the order button is clicked.
-     */
+
+    /**This method is called when the order button is clicked.*/
     public void submitOrder(View view) {
+        CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
+        boolean  hasWhippedCream = whippedCreamCheckBox.isChecked();
+
+        CheckBox chocolateCheckBox = (CheckBox) findViewById(R.id.chocolate_checkbox);
+        boolean hasChocolate = chocolateCheckBox.isChecked();
+
         int price = calculatePrice();
-        String priceMessage =  createOrderSummary();
+        String priceMessage =  createOrderSummary(price, hasWhippedCream, hasChocolate);
         displayMessage(priceMessage);
     }
-    /**
-     * Calculates the price of the order.
-     *
-     * @return total price
-     */
+
+    /**Calculates the price of the order.
+     * @return total price*/
     private int calculatePrice() {
         int price = quantity * 5;
         return price;
     }
-    /**
-     * This posts the submit order summary
-     *
-     * @return text summary
-     */
-    private String createOrderSummary() {
+
+    /**This posts the submit order summary
+     * @param price of the order
+     * @param addWhippedCream is whether or not the user wants whipped cream topping
+     * @param addChocolate is weather or not the user wants chocolate topping
+     * @return text summary*/
+    private String createOrderSummary(int price, boolean addWhippedCream, boolean addChocolate) {
        return "Name: Kaptain Kunal" +
+               "\nAdd Whipped Cream? " + addWhippedCream +
+               "\nAdd Chocolate? " + addChocolate +
                "\nQuantity: " + quantity +
-               "\nTotal: $" + calculatePrice() + ".00" +
+               "\nTotal: $" + price + ".00" +
                "\nThank you!";
     }
 
-    /**
-     * This method is called when the plus button is clicked.
-     */
+    /**This method is called when the plus button is clicked.*/
     public void increment (View view) {
         quantity = quantity + 1;
         displayQuantity(quantity);
     }
-    /**
-     * This method is called when the minus button is clicked.
-     */
+
+    /*** This method is called when the minus button is clicked.*/
     public void decrement (View view) {
         quantity = quantity - 1;
         displayQuantity(quantity);
     }
-    /**
-     * This method displays the given quantity value on the screen.
-     */
+
+    /** This method displays the given quantity value on the screen.*/
     private void displayQuantity(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
-    /**
-     * This method displays the given text on the screen.
-     */
+
+    /**This method displays the given text on the screen.*/
     private void displayMessage(String message) {
         TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
         orderSummaryTextView.setText(message);
