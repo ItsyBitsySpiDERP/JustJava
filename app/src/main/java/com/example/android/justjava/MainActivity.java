56a8d93ab696e96758e762dd667afcd6d0ed2929
Kristen Vincenti <37001304+ItsyBitsySpiDERP@users.noreply.github.com>
2018-08-30 01:41:42 -0400

    Update to quantity
    There are now limits to order quantity. Toast pop ups and added cost for additional toppings.

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -4,14 +4,14 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
-
-import java.text.NumberFormat;
+import android.widget.Toast;
 
 
 /**This app displays an order form to order coffee.*/
 public class MainActivity extends AppCompatActivity {
-    int quantity = 0;
+    int quantity = 1;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,25 +26,45 @@ public class MainActivity extends AppCompatActivity {
         CheckBox chocolateCheckBox = (CheckBox) findViewById(R.id.chocolate_checkbox);
         boolean hasChocolate = chocolateCheckBox.isChecked();
 
-        int price = calculatePrice();
-        String priceMessage =  createOrderSummary(price, hasWhippedCream, hasChocolate);
+        EditText nameField = (EditText) findViewById(R.id.name_field);
+        String name = nameField.getText().toString();
+
+        int price = calculatePrice(hasWhippedCream, hasChocolate);
+        String priceMessage =  createOrderSummary(price, hasWhippedCream, hasChocolate, name);
         displayMessage(priceMessage);
     }
 
     /**Calculates the price of the order.
+     * @param addWhippedCream is weather or not the user wants whipped cream topping
+     * @param addChocolate is whether or not the user wants chocolate topping
      * @return total price*/
-    private int calculatePrice() {
-        int price = quantity * 5;
-        return price;
+    private int calculatePrice(boolean addWhippedCream, boolean addChocolate) {
+        //Price of 1 cup of coffee
+        int basePrice = 5;
+
+        //Add $1 if the user wants whipped cream
+        if (addWhippedCream) {
+            basePrice = basePrice + 1;
+        }
+
+        //Add $2 if the user wants chocolate
+        if (addChocolate) {
+            basePrice = basePrice + 2;
+        }
+
+        //Calculate the total order price by multiplying by quantity.
+        return quantity * basePrice;
+
     }
 
     /**This posts the submit order summary
      * @param price of the order
      * @param addWhippedCream is whether or not the user wants whipped cream topping
      * @param addChocolate is weather or not the user wants chocolate topping
+     * @param name is where a customer enters their name
      * @return text summary*/
-    private String createOrderSummary(int price, boolean addWhippedCream, boolean addChocolate) {
-       return "Name: Kaptain Kunal" +
+    private String createOrderSummary(int price, boolean addWhippedCream, boolean addChocolate, String name) {
+       return "Name: " + name +
                "\nAdd Whipped Cream? " + addWhippedCream +
                "\nAdd Chocolate? " + addChocolate +
                "\nQuantity: " + quantity +
@@ -53,15 +73,25 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**This method is called when the plus button is clicked.*/
-    public void increment (View view) {
+    public int increment (View view) {
         quantity = quantity + 1;
         displayQuantity(quantity);
+        if (quantity >= 100) {
+            quantity = 99;
+            Toast.makeText(this, "You cannot have more than 100 coffees", Toast.LENGTH_SHORT).show();
+        }
+        return quantity;
     }
 
     /*** This method is called when the minus button is clicked.*/
-    public void decrement (View view) {
+    public int decrement (View view) {
         quantity = quantity - 1;
         displayQuantity(quantity);
+        if (quantity <= 0){
+            quantity = 1;
+            Toast.makeText(this, "You cannot have less than 0 coffees", Toast.LENGTH_SHORT).show();
+        }
+        return quantity;
     }
 
     /** This method displays the given quantity value on the screen.*/
